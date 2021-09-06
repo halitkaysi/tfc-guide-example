f1c70abbec87540d388cba56d4129b94584a7448
halitkaysi <halit.kaysi@veolia.com>
2021-09-06 15:44:06 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "instance_type" {
 }
 
 variable "instance_name" {
-  description = "EC2 instance name"
+  description = "EC2 instance name 02"
   default     = "Provisioned by Terraform"
 }
 
